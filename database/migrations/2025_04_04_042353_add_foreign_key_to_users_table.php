2d4c5f45c9fb490990de871a9a0edba7fae40e5e
decemae <decemaerobles69@gmail.com>
2025-04-04 12:42:45 +0800

    2nd commit 04/04/25

--- database/migrations/2025_04_04_042353_add_foreign_key_to_users_table.php
+++ database/migrations/2025_04_04_042353_add_foreign_key_to_users_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->foreignId('role_id')->constrained('roles')->onDelete('restrict');
+            $table->foreignId('role_id')->reference('id')->on('roles')->onDelete('restrict');
             //
         });
     }
