0966303be604c637b4ceed9037696b353cd4fa2b
decemae <decemaerobles69@gmail.com>
2025-04-25 12:36:32 +0800

    add seeder 04/25/25

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use DB;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('roles')->insert([
+            ['role_name' => 'Admin', 'url' => '/admin'],
+            ['role_name' => 'User', 'url' => '/user'],
+            ['role_name' => 'Guest', 'url' => '/guest'],
+        ]);
+        
+    }
+}
